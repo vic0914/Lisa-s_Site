6a7ffc9d30c24486e0c68b62cc08e7f3a9404e88
Victor Rabadan <vrabadan@asu.edu>
2025-09-05 13:37:30 -0700

    Changed order of images on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,28 +6,32 @@ const Home = () => {
   const inProgress = [
     {
       type: "pair",
-      title: "Empress of the Unseen War",
+      title: "Shield Maiden: The Awakening",
       medium: "Colored and Pastel Pencil/Charcoal/Copper Leaf",
       description:
-        "This drawing is a celebration of detail and design. I’m having fun exploring texture, symmetry, and the elegance of traditional adornment.\
-       Each pin and coil is a chance to play with form and precision. It’s a satisfying challenge—balancing beauty and complexity while staying tuned to\
-        the quiet harmony between technique and ornament.",
+        "This piece is still finding its final shape, with copper leaf shimmering and scattering like confetti around the portrait.\
+         The loose flakes and stray brush marks feel like little secrets of the process, as if the painting is dressing up before stepping out into the world. ",
       left: {
-        src: `${import.meta.env.BASE_URL}images/Empress-Process.jpg`,
+        src: `${
+          import.meta.env.BASE_URL
+        }images/Shield-Maiden-Awakening-Process.jpg`,
         alt: "Left image",
       },
       right: {
-        src: `${import.meta.env.BASE_URL}images/Empress-of-the-Unseen-War.jpg`,
+        src: `${import.meta.env.BASE_URL}images/Shield-Maiden-Awakening.jpg`,
         alt: "Right image",
       },
     },
     {
       type: "video",
-      src: `${import.meta.env.BASE_URL}videos/Empress-Process.MOV`,
+      src: `${
+        import.meta.env.BASE_URL
+      }videos/Shield-Maiden-Awakening-Process.mov`,
       title: "Process Video",
       medium: "Short Clip",
       description: "A behind the scenes look.",
     },
+
     {
       type: "pair",
       title: "Shield Maiden",
@@ -53,29 +57,27 @@ const Home = () => {
       medium: "Short Clip",
       description: "A behind the scenes look.",
     },
+
     {
       type: "pair",
-      title: "Shield Maiden: The Awakening",
+      title: "Empress of the Unseen War",
       medium: "Colored and Pastel Pencil/Charcoal/Copper Leaf",
       description:
-        "This piece is still finding its final shape, with copper leaf shimmering and scattering like confetti around the portrait.\
-         The loose flakes and stray brush marks feel like little secrets of the process, as if the painting is dressing up before stepping out into the world. ",
+        "This drawing is a celebration of detail and design. I’m having fun exploring texture, symmetry, and the elegance of traditional adornment.\
+       Each pin and coil is a chance to play with form and precision. It’s a satisfying challenge—balancing beauty and complexity while staying tuned to\
+        the quiet harmony between technique and ornament.",
       left: {
-        src: `${
-          import.meta.env.BASE_URL
-        }images/Shield-Maiden-Awakening-Process.jpg`,
+        src: `${import.meta.env.BASE_URL}images/Empress-Process.jpg`,
         alt: "Left image",
       },
       right: {
-        src: `${import.meta.env.BASE_URL}images/Shield-Maiden-Awakening.jpg`,
+        src: `${import.meta.env.BASE_URL}images/Empress-of-the-Unseen-War.jpg`,
         alt: "Right image",
       },
     },
     {
       type: "video",
-      src: `${
-        import.meta.env.BASE_URL
-      }videos/Shield-Maiden-Awakening-Process.mov`,
+      src: `${import.meta.env.BASE_URL}videos/Empress-Process.MOV`,
       title: "Process Video",
       medium: "Short Clip",
       description: "A behind the scenes look.",
