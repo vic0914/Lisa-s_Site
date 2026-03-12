8f217e3fb3afe37ef07c673165ac71003996d0fb
Victor Rabadan <vrabadan@asu.edu>
2026-03-12 16:10:08 -0700

    Fix base path for dev and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/Lisa-s_Site/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/Lisa-s_Site/" : "/",
   plugins: [react()],
-});
+}));
